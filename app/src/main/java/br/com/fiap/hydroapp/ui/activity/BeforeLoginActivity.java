4b66303016bc0ef4aef77309f9b4c0cf260a3f94
Junior Martins <juniormartins@Juniors-MacBook-Pro.local>
2019-09-01 17:17:10 -0300

    primeiro commi

---
+++ app/src/main/java/br/com/fiap/hydroapp/ui/activity/BeforeLoginActivity.java
@@ -0,0 +1,16 @@
+package br.com.fiap.hydroapp.ui.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import br.com.fiap.hydroapp.R;
+
+public class BeforeLoginActivity extends BaseAppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_before_login);
+
+    }
+}
---
+++ app/src/main/java/br/com/fiap/hydroapp/ui/activity/SignUpActivity.java
@@ -0,0 +1,16 @@
+package br.com.fiap.hydroapp.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.fiap.hydroapp.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
