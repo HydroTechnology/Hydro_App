ee56149430dc53f5ef4c38b82c6ba9cad578873b
Junior Martins <juniormartins@Juniors-MacBook-Pro.local>
2019-09-02 10:47:23 -0300

    Tela Login
    - Tela login

--- app/src/main/java/br/com/fiap/hydroapp/ui/activity/BeforeLoginActivity.java
+++ app/src/main/java/br/com/fiap/hydroapp/ui/activity/BeforeLoginActivity.java
@@ -1,16 +1,56 @@
 package br.com.fiap.hydroapp.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
 
 import br.com.fiap.hydroapp.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class BeforeLoginActivity extends BaseAppCompatActivity {
+
+    @BindView(R.id.activity_before_button_sig_in_up)
+    Button registrerButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_before_login);
+        init();
 
     }
+
+    private void init(){
+
+        ButterKnife.bind(this);
+        setEvents();
+    }
+
+
+    private void setEvents(){
+
+        registrerButton.setOnClickListener(onSaveClick());
+    }
+
+    private View.OnClickListener onSaveClick() {
+
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                signIn();
+
+            }
+        };
+    }
+
+    private void signIn(){
+        Intent intent = new Intent(BeforeLoginActivity.this, SignUpActivity.class);
+        startActivity(intent);
+    }
+
+
 }
